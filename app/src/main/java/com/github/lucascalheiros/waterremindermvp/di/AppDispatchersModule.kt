26d19f253f942ab6e514ea20afc2165dce95c014
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2024-05-11 17:40:34 -0300

    Specify bind class to fix dispatcher named injection

--- app/src/main/java/com/github/lucascalheiros/waterremindermvp/di/AppDispatchersModule.kt
+++ app/src/main/java/com/github/lucascalheiros/waterremindermvp/di/AppDispatchersModule.kt
@@ -1,11 +1,13 @@
 package com.github.lucascalheiros.waterremindermvp.di
 
 import com.github.lucascalheiros.waterremindermvp.common.util.DispatchersQualifier
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dispatchersQualifierModule = module {
-    single(DispatchersQualifier.Main) { Dispatchers.Main }
-    single(DispatchersQualifier.Io) { Dispatchers.IO }
-    single(DispatchersQualifier.Default) { Dispatchers.Default }
+    single(DispatchersQualifier.Main) { Dispatchers.Main } bind CoroutineDispatcher::class
+    single(DispatchersQualifier.Io) { Dispatchers.IO } bind CoroutineDispatcher::class
+    single(DispatchersQualifier.Default) { Dispatchers.Default } bind CoroutineDispatcher::class
 }
