5dc1ee0b12cd877d424befe6596fdf8294b124e7
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2024-05-11 11:48:58 -0300

    Add coercive operators to base interface

--- common/measureSystem/src/main/java/com/github/lucascalheiros/waterremindermvp/common/measuresystem/MeasureSystemVolume.kt
+++ common/measureSystem/src/main/java/com/github/lucascalheiros/waterremindermvp/common/measuresystem/MeasureSystemVolume.kt
@@ -5,16 +5,38 @@ import com.github.lucascalheiros.waterremindermvp.common.measuresystem.impl.Meas
 
 interface MeasureSystemVolume {
     fun intrinsicValue(): Double
+
     fun volumeUnit(): MeasureSystemVolumeUnit
+
     fun toUnit(unit: MeasureSystemVolumeUnit): MeasureSystemVolume
+
     fun toUnit(unit: MeasureSystemUnit): MeasureSystemVolume = toUnit(unit.toVolumeUnit())
+
     operator fun unaryMinus(): MeasureSystemVolume
+
     fun plus(value: MeasureSystemVolume, at: MeasureSystemVolumeUnit): MeasureSystemVolume
-    fun plus(value: MeasureSystemVolume, at: MeasureSystemUnit): MeasureSystemVolume = plus(value, at.toVolumeUnit())
+
+    fun plus(value: MeasureSystemVolume, at: MeasureSystemUnit): MeasureSystemVolume =
+        plus(value, at.toVolumeUnit())
+
     fun minus(value: MeasureSystemVolume, at: MeasureSystemVolumeUnit): MeasureSystemVolume
-    fun minus(value: MeasureSystemVolume, at: MeasureSystemUnit): MeasureSystemVolume = minus(value, at.toVolumeUnit())
-    operator fun <T: Number> times(value: T): MeasureSystemVolume
-    operator fun <T: Number> div(value: T): MeasureSystemVolume
+
+    fun minus(value: MeasureSystemVolume, at: MeasureSystemUnit): MeasureSystemVolume =
+        minus(value, at.toVolumeUnit())
+
+    operator fun <T : Number> times(value: T): MeasureSystemVolume
+
+    operator fun <T : Number> div(value: T): MeasureSystemVolume
+
+    operator fun plus(other: MeasureSystemVolume): MeasureSystemVolume = plus(other, volumeUnit())
+
+    operator fun minus(other: MeasureSystemVolume): MeasureSystemVolume = minus(other, volumeUnit())
+
+    operator fun div(other: MeasureSystemVolume): MeasureSystemVolume =
+        div(other.toUnit(volumeUnit()).intrinsicValue())
+
+    operator fun times(other: MeasureSystemVolume): MeasureSystemVolume =
+        times(other.toUnit(volumeUnit()).intrinsicValue())
 
     companion object {
         fun create(
@@ -31,34 +53,24 @@ interface MeasureSystemVolume {
             return MeasureSystemVolumeImpl(intrinsicValue, measureSystemUnit.toVolumeUnit())
         }
 
-        fun max(a: MeasureSystemVolume, b: MeasureSystemVolume, at: MeasureSystemVolumeUnit): MeasureSystemVolume {
+        fun max(
+            a: MeasureSystemVolume,
+            b: MeasureSystemVolume,
+            at: MeasureSystemVolumeUnit
+        ): MeasureSystemVolume {
             return create(
                 a.toUnit(at).intrinsicValue().coerceAtLeast(b.toUnit(at).intrinsicValue()),
                 at
             )
         }
 
-        inline fun <T> Iterable<T>.sumOfAt(at: MeasureSystemVolumeUnit, selector: (T) -> MeasureSystemVolume): MeasureSystemVolume {
+        inline fun <T> Iterable<T>.sumOfAt(
+            at: MeasureSystemVolumeUnit,
+            selector: (T) -> MeasureSystemVolume
+        ): MeasureSystemVolume {
             return map { selector(it) }.reduceOrNull { v1, v2 ->
                 v1.plus(v2, at)
             } ?: create(0.0, at)
         }
     }
-}
-
-// Coercive operators
-operator fun MeasureSystemVolume.plus(other: MeasureSystemVolume): MeasureSystemVolume {
-    return this.plus(other, volumeUnit())
-}
-
-operator fun MeasureSystemVolume.minus(other: MeasureSystemVolume): MeasureSystemVolume {
-    return this.minus(other, volumeUnit())
-}
-
-operator fun MeasureSystemVolume.div(other: MeasureSystemVolume): MeasureSystemVolume {
-    return this.div(other.toUnit(volumeUnit()).intrinsicValue())
-}
-
-operator fun MeasureSystemVolume.times(other: MeasureSystemVolume): MeasureSystemVolume {
-    return this.times(other.toUnit(volumeUnit()).intrinsicValue())
 }
--- common/measureSystem/src/main/java/com/github/lucascalheiros/waterremindermvp/common/measuresystem/MeasureSystemWeight.kt
+++ common/measureSystem/src/main/java/com/github/lucascalheiros/waterremindermvp/common/measuresystem/MeasureSystemWeight.kt
@@ -4,16 +4,38 @@ import com.github.lucascalheiros.waterremindermvp.common.measuresystem.impl.Meas
 
 interface MeasureSystemWeight {
     fun intrinsicValue(): Double
+
     fun weightUnit(): MeasureSystemWeightUnit
+
     fun toUnit(unit: MeasureSystemWeightUnit): MeasureSystemWeight
+
     fun toUnit(unit: MeasureSystemUnit): MeasureSystemWeight = toUnit(unit.toWeightUnit())
+
     operator fun unaryMinus(): MeasureSystemWeight
+
     fun plus(value: MeasureSystemWeight, at: MeasureSystemWeightUnit): MeasureSystemWeight
-    fun plus(value: MeasureSystemWeight, at: MeasureSystemUnit): MeasureSystemWeight = plus(value, at.toWeightUnit())
+
+    fun plus(value: MeasureSystemWeight, at: MeasureSystemUnit): MeasureSystemWeight =
+        plus(value, at.toWeightUnit())
+
     fun minus(value: MeasureSystemWeight, at: MeasureSystemWeightUnit): MeasureSystemWeight
-    fun minus(value: MeasureSystemWeight, at: MeasureSystemUnit): MeasureSystemWeight = minus(value, at.toWeightUnit())
-    operator fun <T: Number> times(value: T): MeasureSystemWeight
-    operator fun <T: Number> div(value: T): MeasureSystemWeight
+
+    fun minus(value: MeasureSystemWeight, at: MeasureSystemUnit): MeasureSystemWeight =
+        minus(value, at.toWeightUnit())
+
+    operator fun <T : Number> times(value: T): MeasureSystemWeight
+
+    operator fun <T : Number> div(value: T): MeasureSystemWeight
+
+    operator fun plus(other: MeasureSystemWeight): MeasureSystemWeight = plus(other, weightUnit())
+
+    operator fun minus(other: MeasureSystemWeight): MeasureSystemWeight = minus(other, weightUnit())
+
+    operator fun div(other: MeasureSystemWeight): MeasureSystemWeight =
+        div(other.toUnit(weightUnit()).intrinsicValue())
+
+    operator fun times(other: MeasureSystemWeight): MeasureSystemWeight =
+        times(other.toUnit(weightUnit()).intrinsicValue())
 
     companion object {
         fun create(
@@ -31,36 +53,25 @@ interface MeasureSystemWeight {
         }
 
 
-        fun max(a: MeasureSystemWeight, b: MeasureSystemWeight, at: MeasureSystemWeightUnit): MeasureSystemWeight {
+        fun max(
+            a: MeasureSystemWeight,
+            b: MeasureSystemWeight,
+            at: MeasureSystemWeightUnit
+        ): MeasureSystemWeight {
             return create(
                 a.toUnit(at).intrinsicValue().coerceAtLeast(b.toUnit(at).intrinsicValue()),
                 at
             )
         }
 
-        inline fun <T> Iterable<T>.sumOfAt(at: MeasureSystemWeightUnit, selector: (T) -> MeasureSystemWeight): MeasureSystemWeight {
+        inline fun <T> Iterable<T>.sumOfAt(
+            at: MeasureSystemWeightUnit,
+            selector: (T) -> MeasureSystemWeight
+        ): MeasureSystemWeight {
             return map { selector(it) }.reduceOrNull { v1, v2 ->
                 v1.plus(v2, at)
             } ?: create(0.0, at)
         }
 
     }
-}
-
-
-// Coercive operators
-operator fun MeasureSystemWeight.plus(other: MeasureSystemWeight): MeasureSystemWeight {
-    return this.plus(other, weightUnit())
-}
-
-operator fun MeasureSystemWeight.minus(other: MeasureSystemWeight): MeasureSystemWeight {
-    return this.minus(other, weightUnit())
-}
-
-operator fun MeasureSystemWeight.div(other: MeasureSystemWeight): MeasureSystemWeight {
-    return this.div(other.toUnit(weightUnit()).intrinsicValue())
-}
-
-operator fun MeasureSystemWeight.times(other: MeasureSystemWeight): MeasureSystemWeight {
-    return this.times(other.toUnit(weightUnit()).intrinsicValue())
 }
