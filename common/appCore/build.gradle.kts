e9c4e58c722d3303932b5fb98d063477126743cf
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2024-05-11 17:41:35 -0300

    Implement base presenter and base fragment

--- common/appCore/build.gradle.kts
+++ common/appCore/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    implementation(projects.common.util)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
