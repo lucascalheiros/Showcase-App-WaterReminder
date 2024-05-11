83a9aa73f0781eae3e44578d267d9eacca1c3ee3
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2024-05-11 17:56:15 -0300

    Configure edge to edge

--- app/src/main/java/com/github/lucascalheiros/waterremindermvp/ui/MainActivity.kt
+++ app/src/main/java/com/github/lucascalheiros/waterremindermvp/ui/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.github.lucascalheiros.waterremindermvp.ui
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.github.lucascalheiros.waterremindermvp.R
 import com.github.lucascalheiros.waterremindermvp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,14 +23,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.home_nav_graph, R.id.history_nav_graph, R.id.settings_nav_graph
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
