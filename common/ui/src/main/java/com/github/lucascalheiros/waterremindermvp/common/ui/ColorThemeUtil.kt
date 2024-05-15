5006b0d07a9302975900ba37099187b41c66eaaf
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2024-05-15 20:54:51 -0300

    Add dark theme util functions

---
+++ common/ui/src/main/java/com/github/lucascalheiros/waterremindermvp/common/ui/ColorThemeUtil.kt
@@ -0,0 +1,17 @@
+package com.github.lucascalheiros.waterremindermvp.common.ui
+
+import android.content.Context
+import android.content.res.Configuration
+import androidx.annotation.ColorLong
+
+fun Context.isDarkThemeEnabled(): Boolean {
+    return resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+}
+
+fun Context.getThemeAwareColor(@ColorLong lightColor: Long, @ColorLong darkColor: Long): Long {
+    return if (isDarkThemeEnabled()) {
+        darkColor
+    } else {
+        lightColor
+    }
+}
