846a9de60c2852fd771ef877488051321b3329bf
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2024-06-02 18:26:55 -0300

    Add interface to ThemeWrapper to improve unit testing capabilities with dependency injection.

--- data/themeProvider/src/main/java/com/github/lucascalheiros/waterreminder/data/themewrapper/di/ThemeWrapperModule.kt
+++ data/themeProvider/src/main/java/com/github/lucascalheiros/waterreminder/data/themewrapper/di/ThemeWrapperModule.kt
@@ -3,14 +3,16 @@ package com.github.lucascalheiros.waterreminder.data.themewrapper.di
 import android.content.Context
 import com.github.lucascalheiros.waterreminder.data.themewrapper.data.ThemeWrapper
 import com.github.lucascalheiros.waterreminder.data.themewrapper.data.datastore.dataStore
+import com.github.lucascalheiros.waterreminder.data.themewrapper.data.impl.ThemeWrapperImpl
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val themeWrapperModule = module {
     single(themeDataStore) { get<Context>().dataStore }
     single {
-        ThemeWrapper(
+        ThemeWrapperImpl(
             get(themeDataStore),
             get(),
         )
-    }
+    } bind ThemeWrapper::class
 }
